e5c081b70800d3ad8c52efd7c52a19cd5b11b86f
Lauren Boyd <lboyd@esri.com>
2021-06-28 17:15:34 -0700

    Create popup template

--- src/main.js
+++ src/main.js
@@ -10,13 +10,36 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/CSVLayer"], (Map, MapVie
         map: map
     });
 
-    let url = "https://laurenb.esri.com/Personal/SkateParkFinder/LASkateparks.csv";
+    //Create popup template for layer
+    const template = {
+        title: "{Location Name}",
+        content: [{
+            type: "fields",
+            fieldInfos: [{
+                    fieldName: "Address",
+                    label: "Address"
+                },
+                {
+                    fieldName: "Phone",
+                    label: "Phone Number"
+                },
+                {
+                    fieldName: "Website",
+                    label: "Website"
+                }
+            ]
+        }]
+    }
+
+    let url = "https://laurenb.esri.com/Personal/SkateParkFinder/sk8-parks/resources/LASkateparks-June21.csv";
 
     //Create the CSVLayer and set lat/long fields
     let csvLayer = new CSVLayer({
         url: url,
         latitudeField: "lat",
-        longitudeField: "long"
+        longitudeField: "long",
+        copyright: "https://data.lacity.org",
+        popupTemplate: template
     });
 
     //Set view extent once CSVLayer loads
@@ -25,6 +48,8 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/CSVLayer"], (Map, MapVie
     }, function(error) {
         console.log(error);
     });
+
+
 
     map.add(csvLayer);
 });
