a77cd8f618cd829a2c85783c581d1685963f8385
Lauren Boyd <lboyd@esri.com>
2021-08-26 16:45:57 -0700

    Simplify FeatureLayer

--- src/functions/createLayer.js
+++ src/functions/createLayer.js
@@ -1,36 +1,7 @@
-function createFeatureLayer() {
+function createFeatureLayer(template, symbol) {
     let featureLayer;
 
     require(["esri/layers/FeatureLayer"], (FeatureLayer) => {
-
-        //Create popup template for layer
-        const template = {
-            title: "{Location_Name}",
-            content: [{
-                type: "fields",
-                fieldInfos: [{
-                        fieldName: "Address",
-                        label: "Address"
-                    },
-                    {
-                        fieldName: "Phone",
-                        label: "Phone Number"
-                    },
-                    {
-                        fieldName: "Website",
-                        label: "Website"
-                    }
-                ]
-            }]
-        }
-
-        //create symbology from picture marker symbol
-        let symbol = {
-            type: "picture-marker", // autocasts as new PictureMarkerSymbol()
-            url: "https://lboyd93.github.io/sk8-parks/resources/sk8icon.jpg",
-            width: "30px",
-            height: "30px"
-        };
 
         let urlFeatureLayer = "https://services8.arcgis.com/saVeZItOAdgjvI3s/arcgis/rest/services/la_skateparks/FeatureServer/0";
 
@@ -48,29 +19,3 @@ function createFeatureLayer() {
 
     return featureLayer;
 }
-
-// TODO: CVSLayer no longer working when added to this function file - need to check why
-// function createCSVLayer(template, symbol) {
-//     let csvLayer;
-
-//     require(["esri/layers/CSVLayer"], (CSVLayer) => {
-
-//         let urlCSV = "https://lboyd93.github.io/sk8-parks/resources/LASkateparks-June21.csv";
-
-//         //Create the CSVLayer and set lat/long fields
-//         csvLayer = new CSVLayer({
-//             url: urlCSV,
-//             latitudeField: "lat",
-//             longitudeField: "long",
-//             copyright: "https://data.lacity.org",
-//             popupTemplate: template,
-//             renderer: {
-//                 type: "simple",
-//                 symbol: symbol
-//             }
-//         });
-
-//     });
-
-//     return csvLayer;
-// }
--- src/main.js
+++ src/main.js
@@ -10,19 +10,53 @@ require(["esri/Map", "esri/views/MapView", ], (Map, MapView) => {
         map: map
     });
 
-    //add layer depending on source
-    //var layer = addCSVLayer(template, symbol);
-    var layer = createFeatureLayer();
+    //Create popup template for layer
+    const template = {
+        title: "{Location_Name}",
+        content: [{
+            type: "fields",
+            fieldInfos: [{
+                    fieldName: "Address",
+                    label: "Address"
+                },
+                {
+                    fieldName: "Phone",
+                    label: "Phone Number"
+                },
+                {
+                    fieldName: "Website",
+                    label: "Website"
+                }
+            ]
+        }]
+    }
+
+    //create symbology from picture marker symbol
+    let symbol = {
+        type: "picture-marker", // autocasts as new PictureMarkerSymbol()
+        url: "https://lboyd93.github.io/sk8-parks/resources/sk8icon.jpg",
+        width: "30px",
+        height: "30px"
+    };
+
+    //add featurelayer
+    var layer = createFeatureLayer(template, symbol);
     map.add(layer);
 
-    //Set view extent once layer loads
-    layer.when(function() {
-        view.extent = layer.fullExtent;
-        addSearchWidget(view, layer);
-        addDirectionsWidget(view, layer);
-        addFeatureTable(view, layer);
-    }, function(error) {
-        console.log(error);
+    //Set view extent once layer loads and add widgets
+    view.when(function() {
+        view.ui.add("dropdown", {
+            position: "bottom-left"
+        });
+        layer.when(function() {
+            view.extent = layer.fullExtent;
+            addSearchWidget(view, layer);
+            addDirectionsWidget(view, layer);
+            addFeatureTable(view, layer);
+        }, function(error) {
+            console.log(error);
+        });
     });
+
 
 });
