bbdb6444bfe4ee7839112bc111ca6ea8c1daf449
Lauren Boyd <lboyd@esri.com>
2021-08-27 17:19:46 -0700

    Populated dropdown

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ require(["esri/Map", "esri/views/MapView", ], (Map, MapView) => {
         map: map
     });
 
+    const dropdown = document.getElementById("dropdown");
+    dropdown.addEventListener("calciteDropdownSelect", dropdownOnSelectEvent);
+
     //Create popup template for layer
     const template = {
         title: "{Location_Name}",
@@ -45,24 +48,29 @@ require(["esri/Map", "esri/views/MapView", ], (Map, MapView) => {
 
     //Set view extent once layer loads and add widgets
     view.when(function() {
+        //add all UI components when the view loads
         view.ui.add("dropdown", {
             position: "bottom-left"
         });
+        addSearchWidget(view, layer);
+        addDirectionsWidget(view, layer);
+        addFeatureTable(view, layer);
+        //create a layerview to query
         view.whenLayerView(layer).then(function(layerView) {
             view.extent = layerView.layer.fullExtent;
-            addSearchWidget(view, layer);
-            addDirectionsWidget(view, layer);
-            addFeatureTable(view, layer);
             layerView.watch("updating", function(val) {
                 if (!val) { // wait for the layer view to finish updating
                     let query = layerView.createQuery();
+                    //query for all features with all OutFields
                     layerView.queryFeatures(query).then(function(results) {
                         let features = results.features;
                         let locationName = "Location_Name";
+                        //populate the calcite dropdown
                         features.forEach(feature => {
                             //TODO : Get the names to render in the dropdown list
                             const item = document.createElement("calcite-dropdown-item");
-                            item.setAttribute("value", feature.attributes[locationName]);
+                            item.value = feature.attributes[locationName];
+                            item.innerHTML = feature.attributes[locationName];
                             document.getElementById("dropdown").appendChild(item);
                         });
                     });
@@ -74,5 +82,10 @@ require(["esri/Map", "esri/views/MapView", ], (Map, MapView) => {
         });
     });
 
+    function dropdownOnSelectEvent(event) {
+        const target = event.target;
+        const selectItems = target.selectedItems;
+        console.log(selectItems);
+    }
 
 });
