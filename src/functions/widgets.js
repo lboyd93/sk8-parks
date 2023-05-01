7d1cd8acaac0a5bc02c248264220897df41539db
Lauren Boyd <lboyd@esri.com>
2023-05-01 13:41:02 -0700

    fix(widgets & reactiveUtils): Update widget function and update to reactiveUtils

--- src/functions/widgets.js
+++ src/functions/widgets.js
@@ -1,86 +1,97 @@
-function addSearchWidget(view, layer) {
-    require(["esri/widgets/Search"], (Search) => {
-        //add Search Widget to search for the skate park locations
-        const searchWidget = new Search({
-            view: view,
-            apiKey: "CHANGEAPIKEY",
-            includeDefaultSources: false,
-            sources: [{
-                layer: layer,
-                searchFields: ["Location_Name"],
-                displayField: "Location_Name",
-                placeholder: "example: Stoner Skate Park",
-                zoomScale: 20
-            }]
-        });
-        // Adds the Search widget below other elements in
-        // the top right corner of the view
-        view.ui.add(searchWidget, {
-            position: "top-left",
-            index: 0
-        });
+function addWidgets(view, layer, routeLayer) {
+	require([
+		"esri/widgets/Search",
+		"esri/widgets/Directions",
+		"esri/widgets/FeatureTable",
+		"esri/widgets/Expand",
+	], (Search, Directions, FeatureTable, Expand) => {
+		//add Search Widget to search for the skate park locations
+		const searchWidget = new Search({
+			view: view,
+			apiKey: "CHANGEAPIKEY",
+			includeDefaultSources: false,
+			sources: [
+				{
+					layer: layer,
+					searchFields: ["Location_Name"],
+					displayField: "Location_Name",
+					placeholder: "Example: Imperial Courts Skate Park",
+					zoomScale: 20,
+				},
+			],
+		});
 
-    });
-}
+		const searchExpand = new Expand({
+			view: view,
+			content: searchWidget,
+		});
+		// Adds the Search widget below other elements in
+		// the top right corner of the view
+		view.ui.add(searchExpand, {
+			position: "top-left",
+			index: 0,
+		});
 
-function addDirectionsWidget(view, layer) {
-    require(["esri/widgets/Directions"], (Directions) => {
-        const directionsWidget = new Directions({
-            view: view,
-            //apiKey: "CHANGEAPIKEY"
-            searchProperties: {
-                sources: [{
-                    layer: layer,
-                    searchFields: ["Location_Name"],
-                    displayField: "Location_Name",
-                    placeholder: "Imperial Courts Skate Park",
-                    filter: {
-                        geometry: layer.fullExtent
-                    }
-                }]
-            }
-        });
-        // Adds the Directions widget below other elements in
-        // the top right corner of the view
-        view.ui.add(directionsWidget, {
-            position: "top-right",
-            index: 0
-        });
-    });
-}
+		const directionsWidget = new Directions({
+			view: view,
+			//apiKey: "CHANGEAPIKEY"
+			searchProperties: {
+				sources: [
+					{
+						layer: layer,
+						searchFields: ["Location_Name"],
+						displayField: "Location_Name",
+						placeholder: "Example: Imperial Courts Skate Park",
+						filter: {
+							geometry: layer.fullExtent,
+						},
+					},
+				],
+			},
+			layer: routeLayer,
+		});
 
+		const directionsExpand = new Expand({
+			view: view,
+			content: directionsWidget,
+		});
 
-function addFeatureTable(view, layer) {
-    require(["esri/widgets/FeatureTable"], (FeatureTable) => {
-        //Set up feature table and fields to display
-        //Add to div already created
-        const featureTable = new FeatureTable({
-            view: view,
-            layer: layer,
-            attachmentsEnabled: true,
-            fieldConfigs: [{
-                    name: "Location_Name",
-                    label: "Skate Park",
-                    direction: "asc"
-                },
-                {
-                    name: "Address",
-                    label: "Address"
-                },
-                {
-                    name: "Website",
-                    label: "Website"
-                },
-                {
-                    name: "Phone",
-                    label: "Phone Number"
-                },
-                {
-                    name: "CouncilDistrict",
-                    label: "Council District"
-                }
-            ],
-            container: "tableDiv"
-        });
-    });
+		// Adds the Directions widget below other elements in
+		// the top right corner of the view
+		view.ui.add(directionsExpand, {
+			position: "top-right",
+		});
+
+		//Set up feature table and fields to display
+		//Add to div already created
+		const featureTable = new FeatureTable({
+			view: view,
+			layer: layer,
+			attachmentsEnabled: true,
+			fieldConfigs: [
+				{
+					name: "Location_Name",
+					label: "Skate Park",
+					direction: "asc",
+				},
+				{
+					name: "Address",
+					label: "Address",
+				},
+				{
+					name: "Website",
+					label: "Website",
+				},
+				{
+					name: "Phone",
+					label: "Phone Number",
+				},
+				{
+					name: "CouncilDistrict",
+					label: "Council District",
+				},
+			],
+			container: "tableDiv",
+		});
+	});
 }
--- src/main.js
+++ src/main.js
@@ -1,91 +1,101 @@
-require(["esri/Map", "esri/views/MapView", ], (Map, MapView) => {
+require([
+	"esri/Map",
+	"esri/views/MapView",
+	"esri/layers/RouteLayer",
+	"esri/core/reactiveUtils",
+], (Map, MapView, RouteLayer, reactiveUtils) => {
+	const routeLayer = new RouteLayer();
+	//Create the map & mapview
+	const map = new Map({
+		basemap: "topo-vector",
+		layers: [routeLayer],
+	});
 
-    //Create the map & mapview
-    const map = new Map({
-        basemap: "topo-vector"
-    });
+	const view = new MapView({
+		container: "viewDiv",
+		map: map,
+	});
 
-    const view = new MapView({
-        container: "viewDiv",
-        map: map
-    });
+	const dropdown = document.getElementById("dropdown");
+	dropdown.addEventListener("calciteDropdownSelect", dropdownOnSelectEvent);
 
-    const dropdown = document.getElementById("dropdown");
-    dropdown.addEventListener("calciteDropdownSelect", dropdownOnSelectEvent);
+	//Create popup template for layer
+	const template = {
+		title: "{Location_Name}",
+		content: [
+			{
+				type: "fields",
+				fieldInfos: [
+					{
+						fieldName: "Address",
+						label: "Address",
+					},
+					{
+						fieldName: "Phone",
+						label: "Phone Number",
+					},
+					{
+						fieldName: "Website",
+						label: "Website",
+					},
+				],
+			},
+		],
+	};
 
-    //Create popup template for layer
-    const template = {
-        title: "{Location_Name}",
-        content: [{
-            type: "fields",
-            fieldInfos: [{
-                    fieldName: "Address",
-                    label: "Address"
-                },
-                {
-                    fieldName: "Phone",
-                    label: "Phone Number"
-                },
-                {
-                    fieldName: "Website",
-                    label: "Website"
-                }
-            ]
-        }]
-    }
+	//create symbology from picture marker symbol
+	const symbol = {
+		type: "picture-marker", // autocasts as new PictureMarkerSymbol()
+		url: "https://lboyd93.github.io/sk8-parks/resources/sk8icon.jpg",
+		width: "30px",
+		height: "30px",
+	};
 
-    //create symbology from picture marker symbol
-    const symbol = {
-        type: "picture-marker", // autocasts as new PictureMarkerSymbol()
-        url: "https://lboyd93.github.io/sk8-parks/resources/sk8icon.jpg",
-        width: "30px",
-        height: "30px"
-    };
+	//add featurelayer
+	const layer = createFeatureLayer(template, symbol);
+	map.add(layer);
 
-    //add featurelayer
-    const layer = createFeatureLayer(template, symbol);
-    map.add(layer);
+	//Set view extent once layer loads and add widgets
+	view.when(() => {
+		//add all UI components when the view loads
+		view.ui.add("dropdown", {
+			position: "bottom-left",
+		});
+		addWidgets(view, layer, routeLayer);
+		//create a layerview to query
+		view.whenLayerView(layer).then(
+			(layerView) => {
+				view.extent = layerView.layer.fullExtent;
+				reactiveUtils.watch(
+					() => !layerView.updating,
+					() => {
+						// wait for the layer view to finish updating
+						let query = layerView.createQuery();
+						//query for all features with all OutFields
+						layerView.queryFeatures(query).then((results) => {
+							let features = results.features;
+							let locationName = "Location_Name";
+							//populate the calcite dropdown
+							features.forEach((feature) => {
+								//TODO : Get the names to render in the dropdown list
+								const item = document.createElement("calcite-dropdown-item");
+								item.value = feature.attributes[locationName];
+								item.innerHTML = feature.attributes[locationName];
+								document.getElementById("dropdown").appendChild(item);
+							});
+						});
+					}
+				);
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	});
 
-    //Set view extent once layer loads and add widgets
-    view.when(() => {
-        //add all UI components when the view loads
-        view.ui.add("dropdown", {
-            position: "bottom-left"
-        });
-        addSearchWidget(view, layer);
-        addDirectionsWidget(view, layer);
-        addFeatureTable(view, layer);
-        //create a layerview to query
-        view.whenLayerView(layer).then((layerView) => {
-            view.extent = layerView.layer.fullExtent;
-            layerView.watch("updating", (val) => {
-                if (!val) { // wait for the layer view to finish updating
-                    let query = layerView.createQuery();
-                    //query for all features with all OutFields
-                    layerView.queryFeatures(query).then((results) => {
-                        let features = results.features;
-                        let locationName = "Location_Name";
-                        //populate the calcite dropdown
-                        features.forEach((feature) => {
-                            //TODO : Get the names to render in the dropdown list
-                            const item = document.createElement("calcite-dropdown-item");
-                            item.value = feature.attributes[locationName];
-                            item.innerHTML = feature.attributes[locationName];
-                            document.getElementById("dropdown").appendChild(item);
-                        });
-                    });
-                }
-            });
-
-        }, (error)=> {
-            console.log(error);
-        });
-    });
-
-    function dropdownOnSelectEvent(event) {
-        const target = event.target;
-        const selectItems = target.selectedItems;
-        console.log(target);
-    }
-
+	function dropdownOnSelectEvent(event) {
+		const target = event.target;
+		const selectItems = dropdown.selectedItems;
+		console.log(selectItems);
+	}
 });
